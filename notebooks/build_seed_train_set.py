df2d1917deae813bfe26d91d3d8e08dddf0c1b9a
Bruno Camino <camino.bruno@gmail.com>
2025-09-04 10:51:44 +0100

    Resync build_seed_train_set notebook and script

--- notebooks/build_seed_train_set.py
+++ notebooks/build_seed_train_set.py
@@ -10,13 +10,27 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.17.3
 #   kernelspec:
-#     display_name: mace-test
+#     display_name: mace_fto
 #     language: python
 #     name: python3
 #   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
 #     name: python
-#     version: 3.11.10
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.11.13
 # ---
 
 # %% [markdown]
 # # Build the seed train and test sets
+
+# %%
+import sys
+sys.path.append('../src')   # add src/ to Python path
+from functions import *
+
+# %%
