9b970ae0065d6cd3982f5c777be96b5b6e822a7a
Bruno Camino <camino.bruno@gmail.com>
2025-09-04 10:42:54 +0100

    Set up clean nb workflow: pair notebooks only; exclude src/*.py

---
+++ notebooks/build_seed_train_set.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: kernelspec,language_info
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.3
+#   kernelspec:
+#     display_name: mace-test
+#     language: python
+#     name: python3
+#   language_info:
+#     name: python
+#     version: 3.11.10
+# ---
+
+# %% [markdown]
+# # Build the seed train and test sets
